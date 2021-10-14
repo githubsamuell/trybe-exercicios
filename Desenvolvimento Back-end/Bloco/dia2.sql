f140faa2f8b28d558278a7f46b4551104567d708
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2021-10-14 17:51:46 -0300

    resolvendo requisito do 1 ao 5 dia 2 back-end

---
+++ Desenvolvimento Back-end/Bloco/dia2.sql
@@ -0,0 +1,8 @@
+USE Scientists;
+SELECT * FROM Scientists;
+USE Scientists;
+SELECT 'This is SQL Exercise, Practice and Solution';
+SELECT 1, 2, 3;
+SELECT 10 + 15;
+SELECT 10 + 2 * 4;
+SELECT * FROM Scientists;
