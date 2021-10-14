f2c83360f19423e835f911b0843c8d45a166a09f
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2021-10-14 18:07:55 -0300

    resolvendo requisito do 5 ao 10 dia 2 back-end

--- Desenvolvimento Back-end/Bloco/dia2.sql
+++ Desenvolvimento Back-end/Bloco/dia2.sql
@@ -6,3 +6,9 @@ SELECT 1, 2, 3;
 SELECT 10 + 15;
 SELECT 10 + 2 * 4;
 SELECT * FROM Scientists;
+SELECT Name AS 'Nome do Projeto', Hours AS 'Tempo de Trabalho' FROM Projects;
+SELECT Name FROM Scientists ORDER BY Name ASC;
+SELECT Name FROM Projects ORDER BY Name DESC;
+SELECT CONCAT ('O projeto ',Name,'Precisou de ',Hours,' horas para ser concluido') AS resultado FROM Projects;
+SELECT Name, Hours FROM Projects
+ORDER BY Hours DESC LIMIT 3;
