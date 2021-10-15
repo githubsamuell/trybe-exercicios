630d8355ad28c1f760167727f24034c08406b26c
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2021-10-15 18:09:27 -0300

    resolvendo exercicio do 4 ao 7 dia 3 back end

--- Desenvolvimento Back-end/Bloco/dia3.sql
+++ Desenvolvimento Back-end/Bloco/dia3.sql
@@ -7,8 +7,21 @@ USE PecasFornecedores;
     SELECT * FROM Pecas
     WHERE name LIKE 'gr%';
     SELECT * FROM Fornecimentos 
-    WHERE code = 2;
+    WHERE code = 2 ORDER BY Fornecedor;
     SELECT peca, Fornecedor, Preco FROM Fornecimentos
     WHERE Fornecedor LIKE '%n%';
 
-    
+/* Escreva uma query para exibir todas as informações dos fornecedores que são empresas limitadas (LTDA). Ordene os resultados em ordem alfabética decrescente.
+Escreva uma query para exibir o número de empresas (fornecedores) que contém a letra F no código.
+Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00 . Ordene os resultados por ordem crescente.
+Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019 . */
+
+USE PecasFornecedores;
+    SELECT * FROM Fornecedores
+    WHERE name LIKE '%LTDA' ORDER BY name DESC;
+    SELECT * FROM Fornecedores
+    WHERE code LIKE '%N%';
+    SELECT * FROM Fornecimentos
+    WHERE Preco BETWEEN 15 AND 40 ORDER BY Preco;
+    SELECT * FROM Vendas
+    WHERE order_date BETWEEN '2018-04-15' AND '2019-07-30';
