2b348b6eb5cf912db5f2397c5fc2d704bec42c0a
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2021-10-20 19:15:44 -0300

    resolvendo exercicios do 6 ao 10 bloco20 dia 1

--- Desenvolvimento Back-end/bloco20/dia1.sql
+++ Desenvolvimento Back-end/bloco20/dia1.sql
@@ -24,7 +24,7 @@ SELECT
     SUM(SALARY) AS TOTAL_GASTO
 FROM
     employees;
-    
+
 SELECT 
     MAX(SALARY) AS MAIOR_SALARIO,
     MIN(SALARY) AS MENOR_SALARIO,
@@ -32,3 +32,41 @@ SELECT
     AVG(SALARY) AS MEDIA_TODOS
 FROM
     employees; 
+
+-- 6. Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras ( IT_PROG ).
+-- 7. Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão ( JOB_ID ).
+-- 8. Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras ( IT_PROG ).
+-- 9. Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos, exceto das pessoas programadoras ( IT_PROG ).
+-- 10. Escreva um query que exiba média salarial e o número de funcionários de todos os departamentos com mais de dez funcionários. Dica: agrupe pelo department_id .
+SELECT 
+    COUNT(*) AS QPESSOAS_PROGRAMADORAS
+FROM
+    employees
+WHERE
+    JOB_ID = 'IT_PROG';
+SELECT 
+    JOB_ID, SUM(SALARY) AS MONEY_NECESSARY
+FROM
+    employees
+GROUP BY JOB_ID;
+SELECT 
+    JOB_ID, SUM(SALARY) AS MONEY_TO_PAY
+FROM
+    employees
+GROUP BY JOB_ID
+HAVING JOB_ID = 'IT_PROG';
+SELECT 
+    JOB_ID, AVG(SALARY) AS AVG_SALARY
+FROM
+    employees
+GROUP BY JOB_ID
+HAVING JOB_ID <> 'IT_PROG'
+ORDER BY AVG_SALARY DESC;
+SELECT 
+    DEPARTMENT_ID,
+    AVG(SALARY) AS AVG_SALARY,
+    COUNT(DISTINCT EMAIL) AS Q_EMPLOYEES
+FROM
+    employees
+GROUP BY DEPARTMENT_ID
+HAVING Q_EMPLOYEES > 10; 
