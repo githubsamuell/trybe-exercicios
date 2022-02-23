c3afd0a11a1219a490b40b4ff5afcda224b3f564
samuel <samuel.guilherme.l@hotmail.com>
2022-02-23 16:45:29 -0300

    exercicio 3

--- Desenvolvimento Back-end/bloco26/dia2/exercicios/exercicios.ts
+++ Desenvolvimento Back-end/bloco26/dia2/exercicios/exercicios.ts
@@ -1,22 +1,87 @@
-enum MesesDoAno {
-    janeiro= 1,
-    feveiro = 2,
-    março = 3,
-    abril = 4,
-    maio = 5,
-    junho = 6,
-    julho = 7,
-    agosto = 8,
-    setembro = 9,
-    outubro = 10,
-    novembro = 11,
-    dezembro = 12,
+import readline from 'readline-sync';
+import { exit } from "process";
+enum Months {
+    JANEIRO = "Janeiro",
+    FEVEREIRO = "Fevereiro",
+    MARCO = "Março",
+    ABRIL = "Abril",
+    MAIO = "Maio",
+    JUNHO = "Junho",
+    JULHO = "Julho",
+    AGOSTO = "Agosto",
+    SETEMBRO = "Setembro",
+    OUTUBRO = "Outubro",
+    NOVEMBRO = "Novembro",
+    DEZEMBRO = "Dezembro",
+}
 
-};
+enum Seasons {
+    OUTONO = "Outono",
+    INVERNO = "Inverno",
+    PRIMAVERA = "Primavera",
+    VERAO = "Verão",
+}
 
-enum EstaçãoClimaticaDoAno {
-    Outono = 1,
-    Inverno,
-    Primavera,
-    Verão,
-};
+
+
+const monthsNames = Object.values(Months).
+    map(item => item);
+
+const choiceMonth = readline.keyInSelect(monthsNames, "Escolha um mês do ano", { cancel: "SAIR" });
+// O método .keyInSelect mostra uma interface
+// de escolha para a pessoa usuária
+
+if (choiceMonth === -1) {
+    console.log("Saindo!");
+    exit();
+}
+
+const seasonsSouth = {
+    [Seasons.OUTONO]: [Months.MARCO, Months.ABRIL, Months.MAIO, Months.JUNHO],
+    [Seasons.INVERNO]: [Months.JUNHO, Months.JULHO, Months.AGOSTO, Months.SETEMBRO],
+    [Seasons.PRIMAVERA]: [Months.SETEMBRO, Months.OUTUBRO, Months.NOVEMBRO, Months.DEZEMBRO],
+    [Seasons.VERAO]: [Months.DEZEMBRO, Months.JANEIRO, Months.FEVEREIRO, Months.MARCO],
+}
+// Criamos um objeto onde suas chaves são as estações
+// e seus valores são os meses. Note abaixo um segundo
+// objeto onde, sendo do hemisfério oposto, as estações
+// são opostas também
+
+const seasonsNorth = {
+    [Seasons.PRIMAVERA]: seasonsSouth[Seasons.OUTONO],
+    [Seasons.VERAO]: seasonsSouth[Seasons.INVERNO],
+    [Seasons.OUTONO]: seasonsSouth[Seasons.PRIMAVERA],
+    [Seasons.INVERNO]: seasonsSouth[Seasons.VERAO],
+}
+
+const hemispheres = {
+    "Norte": seasonsNorth,
+    "Sul": seasonsSouth
+}
+
+const choiceHemisphere = readline.keyInSelect(Object.keys(hemispheres), "Escolha um hemisfério", { cancel: "SAIR" });
+// O método .keyInSelect mostra uma interface
+// de escolha para a pessoa usuária
+
+if (choiceHemisphere === -1) {
+    console.log("Saindo!");
+    exit();
+}
+
+const month = Object.values(Months)[choiceMonth];
+
+const hemisphere = Object.keys(hemispheres)[choiceHemisphere];
+
+console.log(`Mês: \n${month}`);
+console.log(`Hemisfério: \n${hemisphere}`);
+console.log(`Estações:`);
+const chosenHemisphereSeasons = Object.values(hemispheres)[choiceHemisphere]
+Object.entries(chosenHemisphereSeasons).map((entry) => {
+    const seasons = entry[0];
+    const months = entry[1];
+    // São os meses de cada estação. Caso esteja
+    // incluído em mais de uma estação, o console
+    // abaixo irá adicionar
+
+    if (months.includes(month)) console.log(seasons);
+});
