20a73be3c3e3a322d8cfb875aad054d084f29514
samuel <samuel.guilherme.l@hotmail.com>
2022-02-23 16:31:29 -0300

    exercicios

---
+++ Desenvolvimento Back-end/bloco26/dia2/exercicios/exercicios.ts
@@ -0,0 +1,15 @@
+enum MesesDoAno {
+    janeiro= 1,
+    feveiro = 2,
+    março = 3,
+    abril = 4,
+    maio = 5,
+    junho = 6,
+    julho = 7,
+    agosto = 8,
+    setembro = 9,
+    outubro = 10,
+    novembro = 11,
+    dezembro = 12,
+
+};
