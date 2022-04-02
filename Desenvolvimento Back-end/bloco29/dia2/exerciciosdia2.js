c4d24ef8819b9164f96ac22830d7580c6021ee68
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2022-04-02 16:42:37 -0300

    dia 2 exercicio 6

--- Desenvolvimento Back-end/bloco29/dia2/exerciciosdia2.js
+++ Desenvolvimento Back-end/bloco29/dia2/exerciciosdia2.js
@@ -12,3 +12,6 @@ db.superheroes.find({"aspects.height": { $lte: 180}}).count();
 
 use("class")
 db.superheroes.find({"aspects.height": { $gte: 200}}).limit(1);
+
+use("class")
+db.superheroes.find({"aspects.height": { $gte: 200}}).count();
