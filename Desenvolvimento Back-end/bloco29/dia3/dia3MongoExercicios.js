fc567788bbf0022fe08448d01b31dd3b640cde6a
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2022-04-03 22:01:47 -0300

    exercicio6

--- Desenvolvimento Back-end/bloco29/dia3/dia3MongoExercicios.js
+++ Desenvolvimento Back-end/bloco29/dia3/dia3MongoExercicios.js
@@ -66,3 +66,20 @@ db.movies.find({ratings: {$elemMatch: {$gte: 100, $lte: 105}}}, {_id: 0, title: 
 
 use("cinema")
 db.movies.find({ratings: {$elemMatch: {$gte: 64, $lte: 105, $mod: [9,0]}}}, {_id: 0, title: 1, ratings: 1}).pretty();
+
+use("cinema")
+db.movies.find(
+  {
+    ratings: {
+      $elemMatch: { $gt: 103 }
+    },
+    category: { $all: ["adventure"] }
+  },
+  {
+    _id: 0,
+    title: 1,
+    ratings: 1,
+    category: 1
+  }
+).pretty();
+
