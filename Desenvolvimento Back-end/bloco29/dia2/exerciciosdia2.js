d7f194ff1ae154a52a8f56e832cc6cbda437621d
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2022-04-03 21:14:05 -0300

    exercicios do 12 ao 19

--- Desenvolvimento Back-end/bloco29/dia2/exerciciosdia2.js
+++ Desenvolvimento Back-end/bloco29/dia2/exerciciosdia2.js
@@ -33,3 +33,24 @@ db.superheroes.find({"aspects.hairColor": { $nin: ["No Hair", "Black"]}}).count(
 
 use("class")
 db.superheroes.find({"aspects.height": { $not: {$gt: 180}}});
+
+use("class")
+db.superheroes.find({$nor: [{race: "Human"}, {"aspects.height": {$gt: 180}}]});
+
+use("class")
+db.superheroes.find({$and:[{$or:[{"aspects.height":180}, {"aspects.height":200}]},{"publisher": "Marvel Comics"}]});
+
+use("class")
+db.superheroes.find({$and: [{"aspects.weight": {$gte: 80, $lte: 100}}, {$or: [{race: "Human"}, {race: "Mutant"}]}, {publisher: {$ne: "DC Comics"}}]});
+
+use("class")
+db.superheroes.countDocuments({race: {$exists: false}});
+
+use("class")
+db.superheroes.countDocuments({"aspects.hairColor": {$exists: true}})
+
+use("class")
+db.superheroes.deleteOne({publisher: "Sony Pictures"});
+
+use("class")
+db.superheroes.deleteMany({publisher: "George Lucas"});
