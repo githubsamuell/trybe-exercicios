5bf801b02f649cec42f3b964e1c9a3ba772f4489
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2022-04-05 17:06:01 -0300

    exercicio 4 ao 10

--- Desenvolvimento Back-end/bloco29/dia5/mongodia5Exer.js
+++ Desenvolvimento Back-end/bloco29/dia5/mongodia5Exer.js
@@ -47,4 +47,41 @@ db.movies.updateOne({title: "Batman"}, {$pull: {category: "adventure"}});
 
 
 use("cinema");
-db.movies.find();
+db.movies.updateOne({title: "Batman"}, {$pop: {category: 1}});
+
+use("cinema");
+db.movies.updateOne({title: "Batman"}, {$addToSet: {category: "action"}});
+
+
+use("cinema");
+db.movies.updateOne({title:"Batman", title: "Home Alone"}, {$push: {category: "90 s"}});
+
+
+use("cinema");
+db.movies.updateOne({title: "Home Alone"}, {$addToSet: {cast: {$each: [{
+  "actor": "Macaulay Culkin",
+  "character": "Kevin"
+},
+{
+  "actor": "Joe Pesci",
+  "character": "Harry"
+},
+{
+  "actor": "Daniel Stern"
+}]}}});
+
+use("cinema");
+db.movies.updateOne({title: "Batman"}, {$addToSet: {cast: {$each: [{
+  "character": "Batman"
+},
+{
+  "character": "Alfred"
+},
+{
+  "character": "Coringa"
+}]}}});
+
+
+
+use("cinema");
+db.movies.find().pretty();
